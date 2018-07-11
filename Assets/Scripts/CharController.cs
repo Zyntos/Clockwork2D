0eed699599800236649d32bfdc377ccb75f7557f
Zyntos <zyrielsleepwalker@gmail.com>
2018-07-11 16:09:03 +0200

    Start to Awake

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -17,10 +17,11 @@ public class CharController : MonoBehaviour {
     public float jumpForce = 700f;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         anim = GetComponent<Animator>();
 	}
 	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 
