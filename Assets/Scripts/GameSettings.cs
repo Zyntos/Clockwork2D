4d9c540b652d8e8fe1ea3eb8d4538597192087f8
Kevin Hagen <kvn.hagen@gmail.com>
2018-08-25 12:53:51 +0200

    GameSettings + Missing metas

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,27 @@
+﻿// Kevin Hagen
+// 19.08.2018
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+[CreateAssetMenu(menuName = "ScriptableSingletons/GameSettings")]
+public class GameSettings : ScriptableObjectSingleton<GameSettings>
+{
+	[Header("Rooms")] public float TileSize;
+	public int RoomWidth;
+	public int RoomHeight;
+	[Range(0, 1)] public float BasicRoomChance;
+	[Range(0, 1)] public float TrapRoomChance;
+	[Range(0, 1)] public float TreasureRoomChance;
+	[Range(0, 1), Tooltip("Does this room contain a chest?")] public float TreasureChestChance;
+
+	[Header("LevelGen")] [Range(0, 1)]
+	public float LeaveEarlyChance;
+
+	protected override void OnCreate()
+	{
+		Debug.Log("Create " + ScriptableName);
+	}
+}
